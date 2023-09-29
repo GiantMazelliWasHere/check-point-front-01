8a8a809dc1d9e860c094e07bc9e52468e819ab81
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 10:48:16 -0300

    adição das files .html e .js

---
+++ script.js
@@ -0,0 +1,29 @@
+// Variaveis
+let nome = "Eduardo Mazelli";
+let idade = 24;
+let status = true;
+let valorProduto = 10.50;
+let listaTimes = ["Chicago Bulls 1995-96", "Los Angeles Lakers 2002-03", "Golden State Warriors 2017-18"];
+
+// Print no console
+console.log(nome);
+console.log(idade);
+console.log(status);
+console.log(valorProduto);
+console.log(listaTimes);
+
+// Números
+let num1 = 20;
+let num2 = 2;
+
+// Operações
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao =  num1 / num2;
+
+// Print no console
+console.log("O resultado da soma é: " + soma);
+console.log("O resultado da subtração é: " + subtracao);
+console.log("O resultado da multiplicação é: " + multiplicacao);
+console.log("O resultado da divisão é: " + divisao);
